45114535e99647c4f0d29031d4abc19560e6691c
vcdm100 <vitaomoura100@gmail.com>
2023-06-19 22:57:02 -0300

    Introdução a manipulação de Strings com python

---
+++ Strings.py
@@ -0,0 +1,28 @@
+#Exemplos de programa que aborda o tipo de dado string em Python
+
+frase = "Olá, sou Victor Cláudio!"
+
+#Declaração de uma String
+print("String original: ");
+print(frase);
+print();
+
+#Acessando caracteres individuais
+print("Acessando caracteres individuais: ");
+print("Primeiro caractere: ", frase[0]);
+print("Último caractere: ", frase[-1]);
+print("PenÚltimo caractere: ", frase[-2]);
+print();
+
+#Mais um exemplo
+s = "banana";
+vc = 'Victor';
+
+print(s, id(s));
+print();
+
+#Exemplo de troca uma letra da palavra
+
+vc = vc.replace("V", "B");
+
+print(vc);
