fa424c95b7a2845bdff697aa711e49e98faf9548
vcdm100 <vitaomoura100@gmail.com>
2023-06-19 22:42:58 -0300

    Ajuste declaração variável

---
+++ Variavel.py
@@ -0,0 +1,13 @@
+#Exemplos de Variáveis com nomeclaturadas ERRADAS
+
+121nome = "Victor";
+
+mai$ = "Maior";
+
+class = "Classe de Python";
+
+and = "e";
+
+nome121 = "Victor";
+
+frase = "Olá, tudo bem";
