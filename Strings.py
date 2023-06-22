1c7fb059ad38a7a60b91c0c0d0b93af993788cfb
vcdm100 <vitaomoura100@gmail.com>
2023-06-21 21:13:48 -0300

    Implementação final de manipulação "String"

--- Strings.py
+++ Strings.py
@@ -9,7 +9,9 @@ print();
 
 #Acessando caracteres individuais
 print("Acessando caracteres individuais: ");
+print("Frase: ", frase);
 print("Primeiro caractere: ", frase[0]);
+print("Segundo caractere: ", frase[1]);
 print("Último caractere: ", frase[-1]);
 print("PenÚltimo caractere: ", frase[-2]);
 print();
@@ -23,22 +25,97 @@ print();
 
 #Exemplo de troca uma letra da palavra
 
-vc = vc.replace("V", "B");
+vcs = vc.replace("V", "B");
 
-print(vc);
+print(vcs); #ou print(vc.replace("V", "B"));
+print();
+
+#Utilizando o input para pegar um nome informado pelo usuário:
+nome = input("Nome: ");
+
+print(nome);
+print();
+
+#Inserindo as variáveis no print com f
+print(f"Bem-vindo, {nome}");
+print(f"Bem-vindo, {7*3}");
+print("Bem-vindo, {}".format(nome));
+print();
+
+#Concatenando strings
+print("Concatenando strings: ");
+
+outraFrase = "Bem-vindo";
+fraseCompleta = frase + ' ' + outraFrase;
+
+print(fraseCompleta);
+print();
+
+#Tamanho da string 
+tamanho = len(frase);
+
+print("Tamanho: ", tamanho);
+print();
+
+#Divindo uma string em sub strings
+print("Divindo a string: ");
+
+palavras = fraseCompleta.split();
+
+print(palavras);
+print();
+
+#Substituindo parte das strings
+print("Substituindo parte das strings: ");
+
+novaFrase = frase.replace("Cláudio", "Python");
+
+print(novaFrase);
+print();
+
+#Convertendo para letras maiúsculas e minúsculas
+print("Convertendo para letras maiúsculas e minúsculas: ");
+
+print("Maiúsculas: ", frase.upper());
+print("Minúsculas: ", frase.lower());
+print();
 
 """ RESULTADO (TERMINAL):
 
 String original: 
 Olá, sou Victor Cláudio!
 
-Acessando caracteres individuais: 
+Acessando caracteres individuais:
+Frase:  Olá, sou Victor Cláudio!
 Primeiro caractere:  O
+Segundo caractere:  l
 Último caractere:  !
 PenÚltimo caractere:  o
 
-banana 1840828170672
+banana 2408880350704
 
 Bictor
+
+Nome: Vinicius 
+Vinicius
+
+Bem-vindo, Vinicius
+Bem-vindo, 21
+Bem-vindo, Vinicius
+
+Concatenando strings:
+Olá, sou Victor Cláudio! Bem-vindo
+
+Tamanho:  24
+
+Divindo a string:
+['Olá,', 'sou', 'Victor', 'Cláudio!', 'Bem-vindo']
+
+Substituindo parte das strings:
+Olá, sou Victor Python!
+
+Convertendo para letras maiúsculas e minúsculas:
+Maiúsculas:  OLÁ, SOU VICTOR CLÁUDIO!
+Minúsculas:  olá, sou victor cláudio!
 
 """
