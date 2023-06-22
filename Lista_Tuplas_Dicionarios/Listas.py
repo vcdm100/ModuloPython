f882f88ac3bc58a13d39f6c5376d13ce383fa8e9
vcdm100 <vitaomoura100@gmail.com>
2023-06-22 20:44:02 -0300

    Exemplo de listas

---
+++ Lista_Tuplas_Dicionarios/Listas.py
@@ -0,0 +1,53 @@
+listaNumeros = [1, 2, 3, 4, 5];
+listaStrings = ["a", "b", "c", "d"];
+listaMista = [1, "a", 3.14, True];
+
+print(listaNumeros);
+print(listaStrings);
+print(listaMista);
+print();
+
+frutas = ["maça", "banana", "morango"];
+
+print("gosto de comer da", frutas[1]);
+print();
+
+frutas[1] = "laranja";
+
+print(frutas);
+print("Tamanho 1: ", len(frutas));
+print();
+
+frutas.append("abacaxi");
+
+print(frutas);
+print("Tamanho 2: ", len(frutas));
+print();
+
+listaNova = [1, ["a", "b"]];
+
+print(listaNova[1][0]);
+print();
+
+lista1 = [1,2,3];
+lista2 = [4,5,6];
+
+lisa_concatenada = lista1 + lista2;
+
+print(lisa_concatenada);
+print();
+
+listaRepetida = [0] * 3;
+
+print(listaRepetida);
+print();
+
+numeros = [1,2,3,4];
+numeros2 = ["a", "b", "c", "d"];
+
+sublista = numeros[1:4];
+sublista2 = numeros2[2:4];
+
+print(sublista);
+print(sublista2);
+print();
