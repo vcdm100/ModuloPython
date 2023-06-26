d52db706486b61f829c343e65898e4dd3fe2571e
vcdm100 <vitaomoura100@gmail.com>
2023-06-26 20:47:47 -0300

    Exemplo final de listas

--- Lista_Tuplas_Dicionarios/Listas.py
+++ Lista_Tuplas_Dicionarios/Listas.py
@@ -1,3 +1,4 @@
+#Exemplos de declaração de listas
 listaNumeros = [1, 2, 3, 4, 5];
 listaStrings = ["a", "b", "c", "d"];
 listaMista = [1, "a", 3.14, True];
@@ -7,45 +8,56 @@ print(listaStrings);
 print(listaMista);
 print();
 
+#ACESSANDO A LISTA POR INDICES
 frutas = ["maça", "banana", "morango"];
 
 print("gosto de comer da", frutas[1]);
 print();
 
+#ALTERANDO UM ITEM ESPECIFÍCO DA LISTA
 frutas[1] = "laranja";
 
 print(frutas);
+#IMPRIMINDO O TAMANHO DA LISTA
 print("Tamanho 1: ", len(frutas));
 print();
 
+#ADICIONANDO UM ITEM AO FINAL DA LISTA
 frutas.append("abacaxi");
 
 print(frutas);
 print();
 
+#INSERINDO UM ITEM EM UM INDÍCE ESPECÍFICO DA LISTA
 frutas.insert(0,"uva");
 
 print(frutas);
+#IMPRIMINDO O TAMANHO DA LISTA
 print("Tamanho 2: ", len(frutas));
 print();
 
+#REMOVENDO UM ITEM DA LISTA COM O METODO REMOVE
 frutaRemovida = frutas.remove("morango");
+#REMOVENDO UM ITEM DA LISTA COM O METODO POP
 frutaRemovida = frutas.pop(2);
 
 print(frutas);
 print(frutaRemovida);
 print();
 
+#ORDENANDO UMA LISTA
 frutas.sort();
 
-print("Embaralhado: ", frutas);
+print("Ordenando: ", frutas);
 print();
 
+#ACESSANDO SUBLISTAS
 listaNova = [1, ["a", "b"]];
 
 print(listaNova[1][0]);
 print();
 
+#CONCATENANDO LISTAS
 lista1 = [1,2,3];
 lista2 = [4,5,6];
 
@@ -54,11 +66,13 @@ lisa_concatenada = lista1 + lista2;
 print(lisa_concatenada);
 print();
 
+#DECLARANDO UM VALOR REPETIDO NAS LISTAS
 listaRepetida = [0] * 3;
 
 print(listaRepetida);
 print();
 
+#FATIAMENTO DE LISTAS
 numeros = [1,2,3,4];
 numeros2 = ["a", "b", "c", "d"];
 
@@ -67,6 +81,14 @@ sublista2 = numeros2[2:4];
 
 print(sublista);
 print(sublista2);
+print();
+
+#EMBARALHANDO UMA LISTA
+from random import shuffle
+
+shuffle(frutas);
+
+print("Embaralhado: ", frutas);
 print();
 
 """ RESULTADO (TERMINAL)
@@ -88,7 +110,7 @@ Tamanho 2:  5
 ['uva', 'maça', 'abacaxi']
 laranja
 
-Embaralhado:  ['abacaxi', 'maça', 'uva']
+Ordenando:  ['abacaxi', 'maça', 'uva']
 
 a
 
@@ -98,5 +120,7 @@ a
 
 [2, 3, 4]
 ['c', 'd']
+
+Embaralhado:  ['maça', 'abacaxi', 'uva']
 
 """
