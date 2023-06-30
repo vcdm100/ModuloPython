324f1ca0073c6011a9046ccea47ddbe3e5699502
vcdm100 <vitaomoura100@gmail.com>
2023-06-30 15:33:27 -0300

    Exemplos práticos de Operadores Relacionais

---
+++ Estruturas_Operadores/Operadores_Relacionais.py
@@ -0,0 +1,60 @@
+#Exemplos de Operadores Relacionais
+
+x = 6;
+y = 8;
+
+#Maior que:
+print(x > y);
+print();
+
+""" RESULTADO (TERMINAL)
+
+False
+
+"""
+
+#Menor que:
+print(x < y);
+print();
+
+""" RESULTADO (TERMINAL)
+
+True
+
+"""
+
+#Igualdade:
+print(x == y, "\n");
+
+""" RESULTADO (TERMINAL)
+
+False
+ 
+"""
+
+#Maior ou igual a:
+print(x >= y, "\n");
+
+""" RESULTADO (TERMINAL)
+
+False
+ 
+"""
+
+#Menor ou igual a:
+print(x <= y, "\n");
+
+""" RESULTADO (TERMINAL)
+
+True
+ 
+"""
+
+#Diferente:
+print(x != y, "\n");
+
+""" RESULTADO (TERMINAL)
+
+True
+ 
+"""
