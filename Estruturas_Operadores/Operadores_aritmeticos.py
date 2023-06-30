d68d18ee32889b904536314673b7416cc85e890f
vcdm100 <vitaomoura100@gmail.com>
2023-06-30 14:45:15 -0300

    Exemplos práticos de Operadores Aritméticos

---
+++ Estruturas_Operadores/Operadores_aritmeticos.py
@@ -0,0 +1,33 @@
+#Exemplos de Operadores Aritméticos
+
+a = 15;
+b = 6;
+
+soma            = a + b;
+subtracao       = a - b;
+multiplicacao   = a * b;
+divisao         = a / b;
+resto           = a % b;
+potencia        = a ** b;
+restoDescartado = a // b;
+
+print("Soma: ", soma);
+print("Subtração: ", subtracao);
+print("Multiplicação: ", multiplicacao);
+print("Divisão: ", divisao);
+print("Potência: ", potencia);
+print("Resto da divisão: ", resto);
+print("Divisão após descartartar a parte fracionária: ", restoDescartado);
+print();
+
+""" RESULTADO (TERMINAL)
+
+Soma:  21
+Subtração:  9
+Multiplicação:  90
+Divisão:  2.5
+Potência:  11390625
+Resto da divisão:  3
+Divisão após descartartar a parte fracionária:  2
+
+"""
